f0cfff04b934037d101fd6d71e0b1de9b0d21c0d
djorictijana <djoric.tijana@gmail.com>
2025-04-04 13:15:38 +0200

    Dependency injection extension

---
+++ PuppyParadiseWebAPI/BusinessLogicLayer/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,26 @@
+﻿using BusinessLogicLayer.Services.Implementations;
+using BusinessLogicLayer.Services.Interfaces;
+using DataAccessLayer.Repositories.Implementations;
+using DataAccessLayer.Repositories.Interfaces;
+using DataAccessLayer.UnitOfWork;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped<IUserService, UserService>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            return services;
+        }
+    }
+}
