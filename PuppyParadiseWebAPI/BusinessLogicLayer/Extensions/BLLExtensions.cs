a7989182a3c74671c7295aca4a18bd55a5a64d25
djorictijana <djoric.tijana@gmail.com>
2025-04-06 17:27:13 +0200

    BLL Extensions, DAL Extensions

--- PuppyParadiseWebAPI/BusinessLogicLayer/Extensions/BLLExtensions.cs
+++ PuppyParadiseWebAPI/BusinessLogicLayer/Extensions/BLLExtensions.cs
@@ -1,8 +1,10 @@
 ﻿using BusinessLogicLayer.Services.Implementations;
 using BusinessLogicLayer.Services.Interfaces;
+using DataAccessLayer.Extensions;
 using DataAccessLayer.Repositories.Implementations;
 using DataAccessLayer.Repositories.Interfaces;
 using DataAccessLayer.UnitOfWork;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
@@ -12,13 +14,13 @@ using System.Threading.Tasks;
 
 namespace BusinessLogicLayer.Extensions
 {
-    public static class ServiceCollectionExtensions
+    public static class BLLExtensions
     {
-        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        public static IServiceCollection AddBLLServices(this IServiceCollection services, IConfiguration configuration)
         {
+            services.AddDALServices(configuration);
+
             services.AddScoped<IUserService, UserService>();
-            services.AddScoped<IUserRepository, UserRepository>();
-            services.AddScoped<IUnitOfWork, UnitOfWork>();
 
             return services;
         }
---
+++ PuppyParadiseWebAPI/DataAccessLayer/Extensions/DALExtensions.cs
@@ -0,0 +1,31 @@
+﻿using DataAccessLayer.Context;
+using DataAccessLayer.UnitOfWork;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer.Repositories.Interfaces;
+using DataAccessLayer.Repositories.Implementations;
+
+namespace DataAccessLayer.Extensions
+{
+    public static class DALExtensions
+    {
+        public static IServiceCollection AddDALServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<PuppyParadiseContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            // Registracija repozitorijuma i Unit of Work
+            services.AddScoped<IUnitOfWork, DataAccessLayer.UnitOfWork.UnitOfWork>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            
+
+            return services;
+        }
+    }
+}
