984d81f9c65a065ad7ff3f654b00262ba863b876
djorictijana <djoric.tijana@gmail.com>
2025-04-25 17:30:51 +0200

    DogConfiguration and Migration

---
+++ PuppyParadiseWebAPI/DataAccessLayer/Configurations/DogConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using DomainLayer.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Configurations
+{
+    public class DogConfiguration : IEntityTypeConfiguration<Dog>
+    {
+        public void Configure(EntityTypeBuilder<Dog> builder)
+        {
+            builder.HasOne(d => d.Owner)
+                   .WithOne(du => du.Dog)
+                   .HasForeignKey<DogUser>(du => du.DogId);
+        }
+    }
+}
---
+++ PuppyParadiseWebAPI/DataAccessLayer/Migrations/20250425152939_model10.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class model10 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Dogs_Users_OwnerId",
+                table: "Dogs");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Dogs_OwnerId",
+                table: "Dogs");
+
+            migrationBuilder.CreateTable(
+                name: "DogUser",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DogId = table.Column<int>(type: "int", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DogUser", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_DogUser_Dogs_DogId",
+                        column: x => x.DogId,
+                        principalTable: "Dogs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DogUser_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DogUser_DogId",
+                table: "DogUser",
+                column: "DogId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DogUser_UserId",
+                table: "DogUser",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DogUser");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Dogs_OwnerId",
+                table: "Dogs",
+                column: "OwnerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Dogs_Users_OwnerId",
+                table: "Dogs",
+                column: "OwnerId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
