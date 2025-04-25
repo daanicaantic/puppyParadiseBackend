eaaccfb8fc4280e5b442a504660f853705dd1eb0
djorictijana <djoric.tijana@gmail.com>
2025-04-25 13:03:31 +0200

    WalkingPackageService and WalkingPackageController

--- PuppyParadiseWebAPI/BusinessLogicLayer/Services/Interfaces/IWalkingPackageService.cs
+++ PuppyParadiseWebAPI/BusinessLogicLayer/Services/Interfaces/IWalkingPackageService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DomainLayer.DTOs.WalkingPackageDTOs;
+using DomainLayer.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,14 @@ namespace BusinessLogicLayer.Services.Interfaces
 {
     public interface IWalkingPackageService
     {
+        Task<WalkingPackage> GetWalkingPackageById(int walkingPackageId);
+
+        Task AddWalkingPackage(WalkingPackageWithoutIdDTO walkingPackageWithoutIdDTO);
+
+        Task<List<WalkingPackageDTO>> GetAllWalkingPackages();
+
+        Task UpdateWalkingPackage(WalkingPackageDTO walkingPackageDTO);
+
+        Task DeleteWalkingPackage(int walkingPackageId);
     }
 }
---
+++ PuppyParadiseWebAPI/DomainLayer/DTOs/WalkingPackageDTOs/WalkingPackageWithoutIdDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer.DTOs.WalkingPackageDTOs
+{
+    public class WalkingPackageWithoutIdDTO
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+    }
+}
